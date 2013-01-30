c35569d62bde7ac2639d9ace7aa943b2497dfc3a
cowboysfn007 <cowboysfn007@yahoo.com>
2013-01-30 12:26:51 -0700

    Added additional fields to the user class

--- Dream_Manager/grails-app/domain/dream_manager/User.groovy
+++ Dream_Manager/grails-app/domain/dream_manager/User.groovy
@@ -5,7 +5,16 @@ class User {
 	String firstName
 	String lastName
 	String email
+	String password
 	String avatarLocation
+	String streetAddress1
+	String streetAddress2
+	String poBox
+	String city
+	String state
+	int zipcode
+	
+	
 	
 	// Each User has many Dreams
 	static hasMany = [userToDream:UserToDream]
