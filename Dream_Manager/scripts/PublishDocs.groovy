a6b252089e11bacac55b4d898ca6c2884fccac49
Tom Scott <romzombie@gmail.com>
2013-02-04 13:39:22 -0700

    .

--- Dream_Manager/scripts/PublishDocs.groovy
+++ Dream_Manager/scripts/PublishDocs.groovy
@@ -10,6 +10,19 @@ includeTargets << grailsScript("_GrailsDocs")
 target(main: "Generates documentation and makes it available on gh-pages branch.") {
     remoteUrl = "http://github.com/Dream-Manager/Dream-Manager-Application"
 	
+	// Get current branch name
+	def output = executeGit("branch")
+	def m = output =~ /\*\s+(\S+)/
+	def branch = ""
+	if (m) {
+		branch = m[0][1]
+		println "Current branch: ${branch}"
+	}
+	else {
+		logError("Unable to find out current branch. Output from 'git branch' was:\n\n  ${output}".toString())
+		exit(1)
+	}
+	
 	def docsDir = grailsSettings.docsOutputDir
 	def tmpDocsDir = new File("${basedir}/docs-temp")
 	
@@ -26,13 +39,13 @@ target(main: "Generates documentation and makes it available on gh-pages branch.
 	executeGit("checkout gh-pages")
 	
 	// Copy docs to root folder
-	FileUtils.copyDirectory(tmpDocsDir, "${basedir}")
+	FileUtils.copyDirectory(tmpDocsDir, new File("${basedir}/.."))
 	
-	// Remove temporary copy
+	// Remove temporary copy and project folder
 	ant.delete(dir: tmpDocsDir.absolutePath)
 	
 	// Touch all docs
-	executeGit("add * -f")
+	executeGit("add ../* -f")
 	
 	// Commit to gh-pages
 	executeGit(["commit", "-m", "Auto-publication of docs.", "-a"])
@@ -40,8 +53,8 @@ target(main: "Generates documentation and makes it available on gh-pages branch.
 	// Push to repo
 	executeGit("push")
 	
-	// Change to master branch
-	executeGit("checkout master")
+	// Change to last used branch
+	executeGit("checkout ${branch}")
 }
 
 setDefaultTarget(main)
