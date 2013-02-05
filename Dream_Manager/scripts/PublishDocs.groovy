d78692a4b527e87f96d188c78aad06e41cfaae7c
Tom Scott <romzombie@gmail.com>
2013-02-04 18:09:04 -0700

    Final version of PublishDocs script, clearing documentation out of master

--- Dream_Manager/scripts/PublishDocs.groovy
+++ Dream_Manager/scripts/PublishDocs.groovy
@@ -10,7 +10,6 @@ includeTargets << grailsScript("_GrailsDocs")
 target(main: "Generates documentation and makes it available on gh-pages branch.") {
     remoteUrl = "http://github.com/Dream-Manager/Dream-Manager-Application"
 	
-	/*
 	// Get current branch name
 	def output = executeGit("branch")
 	def m = output =~ /\*\s+(\S+)/
@@ -23,7 +22,6 @@ target(main: "Generates documentation and makes it available on gh-pages branch.
 		logError("Unable to find out current branch. Output from 'git branch' was:\n\n  ${output}".toString())
 		exit(1)
 	}
-	*/
 	
 	def docsDir = grailsSettings.docsOutputDir
 	def tmpDocsDir = new File("${basedir}/docs-temp")
@@ -37,10 +35,8 @@ target(main: "Generates documentation and makes it available on gh-pages branch.
 	// Rename the generated folder to a temporary one
 	docsDir.renameTo(tmpDocsDir)
 	
-	/*
 	// Change to gh-pages branch
 	executeGit("checkout gh-pages")
-	*/
 	
 	// Copy docs to root folder
 	FileUtils.copyDirectory(tmpDocsDir, new File("${basedir}/.."))
@@ -57,10 +53,8 @@ target(main: "Generates documentation and makes it available on gh-pages branch.
 	// Push to repo
 	executeGit("push")
 	
-	/*
 	// Change to last used branch
 	executeGit("checkout ${branch}")
-	*/
 }
 
 setDefaultTarget(main)
