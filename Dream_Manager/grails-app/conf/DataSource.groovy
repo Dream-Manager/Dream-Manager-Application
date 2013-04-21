303597395fc29af90e0075284e50db1c6b1a758c
Tom Scott <romzombie@gmail.com>
2013-04-21 08:17:43 -0700

    Switching database source to MySQL. Change to development environment required, see Contribution docs for details.

--- Dream_Manager/grails-app/conf/DataSource.groovy
+++ Dream_Manager/grails-app/conf/DataSource.groovy
@@ -1,8 +1,7 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+	dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -14,19 +13,28 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/dream_manager_development"
+			username = "dream_manager"
+			password = ""
         }
+		hibernate {
+			show_sql = true
+		}
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/dream_manager_test"
+			username = "dream_manager"
+			password = ""
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/dream_manager"
+			username = "dream_manager"
+			password = ""
             pooled = true
             properties {
                maxActive = -1
