05c3dbbb1c013fa6600779ff8b1b12015a3d6e7b
James Harris <wizardofmath@gmail.com>
2013-04-24 18:07:42 -0700

    Added Java Doc for the managerRequest class

--- Dream_Manager/grails-app/domain/dream_manager/ManagerRequest.groovy
+++ Dream_Manager/grails-app/domain/dream_manager/ManagerRequest.groovy
@@ -1,5 +1,12 @@
 package dream_manager;
 
+/**
+ * This Class is used to represent the request made by either a manager
+ * of dreamer to create a manager dreamer relation. The ManagerToDreamerController
+ * handles all of these requests.
+ * @author James
+ *
+ */
 public class ManagerRequest extends PasswordResetRequest {
 	User manager
 	User requestInitiator
