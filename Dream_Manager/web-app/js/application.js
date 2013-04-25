586189db2fd5f3945d34656f0f1bd015d1cc2ab2
James Harris <wizardofmath@gmail.com>
2013-04-25 11:17:37 -0700

    Added tool tips functionality.
    Add title="Tooltip contents here" inside dive tags or element tag.

--- Dream_Manager/web-app/js/application.js
+++ Dream_Manager/web-app/js/application.js
@@ -5,40 +5,67 @@ if (typeof jQuery !== 'undefined') {
 		}).ajaxStop(function() {
 			$(this).fadeOut();
 		});
-		
+
 		$(".datepicker").datepicker();
 		$(".sortable").sortable();
 		$(".tabs").tabs();
-		$(".accordion").accordion({heightStyle: "content",
-									collapsible: true,
-									active: false
-									});
+		$(".accordion").accordion({
+			heightStyle : "content",
+			collapsible : true,
+			active : false
+		});
 		$(".button").button();
- 		$(".progressbar").each(function(){
-			$this = $(this);
-			percentage = Number($this.text());
-			style = $this.attr("style");
-			$progressbar = $("<div class='activeProgressbar' style='"+style+"'></div>").progressbar({value:percentage});
-			$this.replaceWith($progressbar);
- 	 	});
- 		
- 		$(".slider").each(function(){
-			$this = $(this);
-			$this.css("display","none");
-			percentage = Number($this.val());
-			$slider = $("<div class='activeSlider' style='width:10em;'></div>").slider({
-				min: 0, 
-				max: 100, 
-				value: percentage,
-				change: function (e, ui) {
-					$this.val($(this).slider("option", "value"));
-				}
-			});
-			$this.after($slider);
- 	 	});
- 		$(".slider").parent("form").submit(function(){
- 			$(this).find(".activeSlider").slider("option", "value", 0);
- 		});
+		$(".progressbar").each(
+				function() {
+					$this = $(this);
+					percentage = Number($this.text());
+					style = $this.attr("style");
+					$progressbar = $(
+							"<div class='activeProgressbar' style='" + style
+									+ "'></div>").progressbar({
+						value : percentage
+					});
+					$this.replaceWith($progressbar);
+				});
 
+		$(".slider")
+				.each(
+						function() {
+							$this = $(this);
+							$this.css("display", "none");
+							percentage = Number($this.val());
+							$slider = $(
+									"<div class='activeSlider' style='width:10em;'></div>")
+									.slider(
+											{
+												min : 0,
+												max : 100,
+												value : percentage,
+												change : function(e, ui) {
+													$this.val($(this).slider(
+															"option", "value"));
+												}
+											});
+							$this.after($slider);
+						});
+		$(".slider").parent("form").submit(function() {
+			$(this).find(".activeSlider").slider("option", "value", 0);
+		});
+		{
+			$(document).tooltip({
+				show : {
+					delay : 1000
+				},
+				hide : {
+					effect : "fade",
+					duration : 200
+				},
+				position : {
+					my : "left+15 center",
+					at : "right center"
+				},
+			});
+		}
+		;
 	})(jQuery);
 }
